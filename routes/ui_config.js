10f08a1ddec2382b52a41e0102f007567668ac36
nisarg <hammerbyte.nisarg@gmail.com>
2024-12-19 12:57:28 +0530

    carousel config added

--- routes/ui_config.js
+++ routes/ui_config.js
@@ -16,68 +16,22 @@ router.get("/navbar", async (req, res) => {
     }
 });
 
+//Carousel WIDGET
+router.get("/carousel", async (req, res) => {
+    try {
+        const carouselConfig = await readConfig("carousel");
+        res.status(200).json(carouselConfig);
+    } catch (error) {
+        logger.error(error);
+        res.status(500).json({ error: error });
+    }
+});
+
 //FOOTER WIDGET
 router.get("/footer", async (req, res) => {
     try {
         const footerConfig = await readConfig("footer");
         res.status(200).json(footerConfig);
-    } catch (error) {
-        logger.error(error);
-        res.status(500).json({ error: error });
-    }
-});
-
-//LOGIN CONTAINER
-router.get("/login", async (req, res) => {
-    try {
-        const loginConfig = await readConfig("login");
-        res.status(200).json(loginConfig);
-    } catch (error) {
-        logger.error(error);
-        res.status(500).json({ error: error });
-    }
-});
-
-//DASHBOARD CONTAINER
-router.get("/dashboard", async (req, res) => {
-    try {
-        const dashBoardConfig = await readConfig("dashboard");
-        dashBoardConfig.showcase.product_categories = await executeSQLQueryRaw("SELECT * FROM PRODUCT_CATEGORIES");
-        res.status(200).json(dashBoardConfig);
-    } catch (error) {
-        logger.error(error);
-        res.status(500).json({ error: error });
-    }
-});
-
-//ONE PERTICULAR PRODUCT SECTION
-router.get("/product", async (req, res) => {
-    try {
-        const productConfig = await readConfig("product");
-        res.status(200).json(productConfig);
-    } catch (error) {
-        logger.error(error);
-        res.status(500).json({ error: error });
-    }
-});
-
-//PRODUCTS Section - SEARCH
-router.get("/products", async (req, res) => {
-    try {
-        const productsConfig = await readConfig("products");
-        productsConfig.products_search.product_categories = await executeSQLQueryRaw("SELECT name FROM PRODUCT_CATEGORIES");
-        res.status(200).json(productsConfig);
-    } catch (error) {
-        logger.error(error);
-        res.status(500).json({ error: error });
-    }
-});
-
-//DEMO Section
-router.get("/demo", async (req, res) => {
-    try {
-        const demoConfig = await readConfig("demo");
-        res.status(200).json(demoConfig);
     } catch (error) {
         logger.error(error);
         res.status(500).json({ error: error });

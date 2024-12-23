b2e00c397b269fd77ccd7e4ef908a3580d122c8c
nisarg <hammerbyte.nisarg@gmail.com>
2024-12-24 01:54:56 +0530

    minor bug fix

--- routes/users.js
+++ routes/users.js
@@ -9,13 +9,12 @@ router.patch("/:id/primary-details", async (req, res) => {
         if (req.cookies.token) {
             const user = await getUserByToken(req.cookies.token);
 
-            if (user && updateUserPrimaryDetails(user.id, req.body.name, req.body.phone)) {
+            if (user && (await updateUserPrimaryDetails(user.id, req.body.name, req.body.phone))) {
                 return res.status(200).json(await getUserByToken(req.cookies.token));
             }
         }
         return res.status(401).json({ error: "Missing Authentication" });
     }
-
     res.status(400).json({ error: "Invalid/Missing Parameters" });
 });
 

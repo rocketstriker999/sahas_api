32a0f69aa8038fa538838f0e277f9def07a3385b
nisarg <hammerbyte.nisarg@gmail.com>
2024-12-24 02:47:50 +0530

    fixed minor bug

--- routes/users.js
+++ routes/users.js
@@ -5,17 +5,14 @@ const router = libExpress.Router();
 
 //update user's details before purchase if user is missing primary details
 router.patch("/:id/primary-details", async (req, res) => {
-    if (req.params.id && req.body.name && req.body.phone) {
-        if (req.cookies.token) {
-            const user = await getUserByToken(req.cookies.token);
+    if (req.params.id && req.body.name && req.body.phone && req.cookies.token) {
+        const user = await getUserByToken(req.cookies.token);
 
-            if (user && (await updateUserPrimaryDetails(user.id, req.body.name, req.body.phone))) {
-                return res.status(200).json(await getUserByToken(req.cookies.token));
-            }
+        if (user && (await updateUserPrimaryDetails(user.id, req.body.name, req.body.phone))) {
+            return res.status(200).json(await getUserByToken(req.cookies.token));
         }
-        return res.status(401).json({ error: "Missing Authentication" });
     }
-    res.status(400).json({ error: "Invalid/Missing Parameters" });
+    return res.status(401).json({ error: "Missing Required Information" });
 });
 
 module.exports = router;

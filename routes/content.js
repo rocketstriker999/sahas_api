01358466100d95689a3af0df45a3e667dcf45975
nisarg <hammerbyte.nisarg@gmail.com>
2025-01-05 02:40:11 +0530

    access restriction added

--- routes/content.js
+++ routes/content.js
@@ -1,17 +1,26 @@
 const libExpress = require("express");
 const { getContentById } = require("../db/content");
+const { getAccessByTokenAndContentId } = require("../db/accesses");
+
 const router = libExpress.Router();
 
-router.get("/:id", async (req, res) => {
+router.get("/public/:id", async (req, res) => {
     if (req.params.id) {
-        //if content id is there
-        //check if demo
-        //check if chapter's data
         const content = await getContentById(req.params.id);
-
         return res.status(200).json(content);
     }
     return res.status(400).json({ error: "Missing Content Id" });
+});
+
+router.get("/private/:id", async (req, res) => {
+    if (req.cookies.token && req.params.id) {
+        if (await getAccessByTokenAndContentId(req.cookies.token, req.params.id)) {
+            const content = await getContentById(req.params.id);
+            return res.status(200).json(content);
+        }
+        return res.status(401).json({ error: "You Don't Have access to this content" });
+    }
+    return res.status(400).json({ error: "Missing Required Details" });
 });
 
 module.exports = router;

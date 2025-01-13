05b064914b6474787e3f8de500e98a5b5a9fe066
nisarg <hammerbyte.nisarg@gmail.com>
2025-01-14 01:08:34 +0530

    minor change

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ prepareDirectories([process.env.DIRECTORY_LOGS, process.env.DIR_CONFIGS]);
 // Test and Prepare Required Tables
 generateDBTables()
     .then(() => {
-        logger.success("[+]Tables Ready");
+        logger.success("[+]Database Ready");
         allowTraffic();
     })
-    .catch((error) => logger.error(`[-]Failed To Generate Tables ${error}`));
+    .catch((error) => logger.error(`[-]Failed To Prepare Database ${error}`));

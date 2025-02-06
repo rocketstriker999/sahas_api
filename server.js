86eebe88b53350cc6cb9121ba946d1a97e4ce44b
nisarg <hammerbyte.nisarg@gmail.com>
2025-02-07 03:35:00 +0530

    enable all cors

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 const sahasAPI = libExpress();
 
 // Use the CORS middleware to allow cross origin request in case of testing UI and Backend
-sahasAPI.use(cors({ origin: process.env.ALLOWED_ORIGINS || false }));
+sahasAPI.use(cors());
 
 //sahasAPI.use(require("./middlewares/device"));
 sahasAPI.use((req, res, next) => {

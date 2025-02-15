288047ddb0ca83cb055e87f14d705794f6e078ce
nisarg <hammerbyte.nisarg@gmail.com>
2025-02-15 16:42:44 +0530

    video support added

--- routes/media.js
+++ routes/media.js
@@ -33,7 +33,7 @@ router.get("/subjects/:subjectId/extract/:mediaId", async (req, res) => {
         if (media)
             return requestService({
                 requestServiceName: process.env.SERVICE_MEDIA,
-                requestPath: "extract",
+                requestPath: `extract/${media.type}`,
                 requestMethod: "POST",
                 requestPostBody: media,
                 onResponseReceieved: (sources, responseCode) => res.status(responseCode).json({ sources }),

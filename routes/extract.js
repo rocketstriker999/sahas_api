95fc12b8c4e44264974c90d1f5b2713c6dfe2059
nisarg <hammerbyte.nisarg@gmail.com>
2025-02-22 14:46:29 +0530

    added redirection for cdn services

--- routes/extract.js
+++ routes/extract.js
@@ -27,16 +27,19 @@ router.get("/subjects/:subjectId/:mediaId", async (req, res) => {
 router.get("/chapters/:chapterId/:mediaId", async (req, res) => {
     if (req.params.chapterId && req.params.mediaId) {
         const media = await extractMediaByChapterIdAndMediaId(req.params.chapterId, req.params.mediaId);
-        if (media)
-            return requestService({
-                requestServiceName: process.env.SERVICE_MEDIA,
-                requestPath: `extract`,
-                requestMethod: "POST",
-                requestPostBody: media,
-                onResponseReceieved: (sources, responseCode) => res.status(responseCode).json({ sources }),
-                onRequestFailure: (e) => res.status(500).json({ error: `Media Item Not Ready For Stream ${e}` }),
-            });
-        return res.status(400).json({ error: "Invalid Media Request - Media Not Found" });
+
+        return res.redirect(301, `${process.env.SERVICE_MEDIA}/${media.type}/${media.cdn_id}`);
+
+        // if (media.type === "video")
+        //     return requestService({
+        //         requestServiceName: process.env.SERVICE_MEDIA,
+        //         requestPath: `extract`,
+        //         requestMethod: "POST",
+        //         requestPostBody: media,
+        //         onResponseReceieved: (sources, responseCode) => res.status(responseCode).json({ sources }),
+        //         onRequestFailure: (e) => res.status(500).json({ error: `Media Item Not Ready For Stream ${e}` }),
+        //     });
+        // return res.status(400).json({ error: "Invalid Media Request - Media Not Found" });
     }
     return res.status(400).json({ error: "Missing Required Details" });
 });

b674cb3445ed989b34c335029b43b22e661282e9
Abhishek Jadav <abhijadav99@gmail.com>
2025-02-26 03:14:52 +0530

    Logout issue

--- routes/token.js
+++ routes/token.js
@@ -19,7 +19,11 @@ router.get("/verify", async (req, res) => {
 });
 
 router.post("/invalidate", async (req, res) => {
-    await res.clearCookie("token");
+    await res.clearCookie("token",{
+        httpOnly: true,
+        secure: true,
+        sameSite: "None",
+    });
     return res.status(200).json({
         message: "Token Invalidated",
     });

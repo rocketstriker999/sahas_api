0608e22422f440f20c2c788f102579af23b7718e
Abhishek Jadav <abhijadav99@gmail.com>
2025-04-02 00:50:59 +0530

    testing

--- routes/transaction.js
+++ routes/transaction.js
@@ -34,8 +34,8 @@ router.post("/", async (req, res) => {
                 }
                 transaction.sgst = Number((transaction.pay * process.env.SGST) / 100);
                 transaction.cgst = Number((transaction.pay * process.env.CGST) / 100);
-                transaction.pay = parseFloat(transaction.pay + transaction.sgst + transaction.cgst).toFixed(2);
-                // transaction.discounted = parseFloat(transaction.pay - transaction.sgst - transaction.cgst + transaction.benifit).toFixed(2);
+                //transaction.pay = parseFloat(transaction.pay + transaction.sgst + transaction.cgst).toFixed(2);
+                transaction.discounted = parseFloat(transaction.pay - transaction.sgst - transaction.cgst + transaction.benifit).toFixed(2);
                 transaction.userId = user.id;
                 transaction.payuMerchantKey = process.env.MERCHANT_KEY;
                 transaction.successURL = process.env.TRANSACTION_SUCCESS_URL;

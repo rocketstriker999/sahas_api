26da558f994333feffb666c58dd134d2801c2914
Abhishek Jadav <abhijadav99@gmail.com>
2025-04-09 20:01:28 +0530

    testing success result only

--- db/transactions.js
+++ db/transactions.js
@@ -49,25 +49,26 @@ function getTransactionById(transactionId) {
 
 function getAllTransactionData() {
     console.log("getAllTransaction query function called");
-    return executeSQLQueryParameterized(`SELECT 
-                table_transactions.*, 
-                table_users.name AS user_name, 
-                table_users.email AS user_email, 
-                table_users.phone AS user_phone, 
-                table_users.address AS user_address, 
-                table_products.title AS product_title, 
-                table_products.description AS product_description, 
-                table_products.price AS product_price, 
-                table_products.discounted AS product_discounted, 
-                table_products.access_validity AS product_access_validity 
-            FROM TRANSACTIONS table_transactions 
-            WHERE UPPER(table_transactions.status) = 'SUCCESS'
-            INNER JOIN USERS table_users 
-                ON table_transactions.user_id = table_users.id 
-            INNER JOIN PRODUCTS table_products 
-                ON table_transactions.product_id = table_products.id
-            `)
-        .then((result) => {
+    return executeSQLQueryParameterized(`
+        SELECT 
+            table_transactions.*, 
+            table_users.name AS user_name, 
+            table_users.email AS user_email, 
+            table_users.phone AS user_phone, 
+            table_users.address AS user_address, 
+            table_products.title AS product_title, 
+            table_products.description AS product_description, 
+            table_products.price AS product_price, 
+            table_products.discounted AS product_discounted, 
+            table_products.access_validity AS product_access_validity 
+        FROM TRANSACTIONS table_transactions 
+        INNER JOIN USERS table_users 
+            ON table_transactions.user_id = table_users.id 
+        INNER JOIN PRODUCTS table_products 
+            ON table_transactions.product_id = table_products.id
+        WHERE TRIM(UPPER(table_transactions.status)) = 'SUCCESS';
+    `)
+    .then((result) => {
         console.log("SQL result:", result); 
         return result;
     })

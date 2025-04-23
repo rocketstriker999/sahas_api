caad91a3c874d0d8c97a3f0ea1ff87bb6128cccd
nisarg <hammerbyte.nisarg@gmail.com>
2025-04-24 02:49:22 +0530

    dev deploy

--- routes/extract.js
+++ routes/extract.js
@@ -16,7 +16,7 @@ router.get("/subjects/:subjectId/:mediaId", async (req, res) => {
 router.get("/chapters/:chapterId/:mediaId", async (req, res) => {
     if (req.params.chapterId && req.params.mediaId) {
         const media = await extractMediaByChapterIdAndMediaId(req.params.chapterId, req.params.mediaId);
-        return res.redirect(301, `/${process.env.SERVICE_MEDIA}extract/${media.type}/${media.cdn_id}`);
+        return res.redirect(301, `/${process.env.SERVICE_MEDIA}extract/${media.type}/${media.cdn_id}?p=5`);
     }
     return res.status(400).json({ error: "Missing Required Details" });
 });

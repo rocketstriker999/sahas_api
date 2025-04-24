fd8904172846431720f1aaaace01b1c9ebffc38e
nisarg <hammerbyte.nisarg@gmail.com>
2025-04-24 20:01:31 +0530

    extraction added

--- routes/extract.js
+++ routes/extract.js
@@ -15,24 +15,24 @@ router.get("/subjects/:subjectId/:mediaId", async (req, res) => {
 
 //Chapter Content extractions
 router.get("/chapters/:chapterId/:mediaId", async (req, res) => {
-    if (req.params.chapterId && req.params.mediaId) {
-        const query = new URLSearchParams(req.query).toString();
-        console.log("######################### CALLED", query);
-        const media = await extractMediaByChapterIdAndMediaId(req.params.chapterId, req.params.mediaId);
-
-        requestService({
-            requestServiceName: process.env.SERVICE_MEDIA,
-            requestPath: `extract/video/${media.cdn_id}`,
-            onResponseReceieved: (sources, responseCode) => {
-                if (responseCode === 200 && sources.length) return res.status(200).json(sources);
-                return res.status(500).json({ error: "Error While Generating Sources" });
-            },
-            onRequestFailure: (error) => {
-                res.status(500).json({ error });
-            },
-        });
+    if (!req.params.chapterId || !req.params.mediaId) {
+        return res.status(400).json({ error: "Missing Required Details" });
     }
-    return res.status(400).json({ error: "Missing Required Details" });
+
+    console.log("######################### CALLED", req.query);
+    const media = await extractMediaByChapterIdAndMediaId(req.params.chapterId, req.params.mediaId);
+
+    requestService({
+        requestServiceName: process.env.SERVICE_MEDIA,
+        requestPath: `extract/video/${media.cdn_id}`,
+        onResponseReceieved: (sources, responseCode) => {
+            if (responseCode === 200 && sources.length) return res.status(200).json(sources);
+            return res.status(500).json({ error: "Error While Generating Sources" });
+        },
+        onRequestFailure: (error) => {
+            res.status(500).json({ error });
+        },
+    });
 });
 
 module.exports = router;

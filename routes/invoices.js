fb29cce37d91893b1af6682424d2c15903539c78
nisarg <hammerbyte.nisarg@gmail.com>
2025-05-16 22:25:59 +0530

    regeneration

--- routes/invoices.js
+++ routes/invoices.js
@@ -2,37 +2,35 @@ const libExpress = require("express");
 const { requestService } = require("../utils");
 const { getTransactionByInvoice } = require("../db/transactions");
 const logger = require("../libs/logger");
+const { getUserByTransactionId } = require("../db/users");
+const { getProductById } = require("../db/products");
 
 const router = libExpress.Router();
 
 //update user's details before purchase if user is missing primary details -1
 router.get("/regenrate/:invoice", async (req, res) => {
-    if (req.params.invoice) {
-        logger.info(`Incoming Request - ${req.method} ${req.url}`);
-        const transaction = await getTransactionByInvoice(req.params.invoice);
+    logger.info(`Regenerating Invoice ${req.params.invoice}`);
+    const transaction = await getTransactionByInvoice(req.params.invoice);
 
-        requestService({
-            requestServiceName: process.env.SERVICE_MEDIA,
-            requestPath: "generate/invoice",
-            requestMethod: "POST",
-            requestPostBody: {
-                transaction,
-                user: await getUserByTransactionId(transaction.id),
-                product: await getProductById(transaction.product_id),
-                percent_sgst: process.env.SGST,
-                percent_cgst: process.env.CGST,
-            },
-            onResponseReceieved: (invoice, responseCode) => {
-                return res.status(responseCode).json(invoice);
-            },
-            onRequestFailure: (error) => {
-                logger.error(`Failed To regenerate Invoice for transcation - ${transaction.id} error - ${error}`);
-                return res.status(500).json({ error: "Failed To Generate Invoice" });
-            },
-        });
-    } else {
-        return res.status(400).json({ error: "Missing Invoice Id" });
-    }
+    requestService({
+        requestServiceName: process.env.SERVICE_MEDIA,
+        requestPath: "generate/invoice",
+        requestMethod: "POST",
+        requestPostBody: {
+            transaction,
+            user: await getUserByTransactionId(transaction.id),
+            product: await getProductById(transaction.product_id),
+            percent_sgst: process.env.SGST,
+            percent_cgst: process.env.CGST,
+        },
+        onResponseReceieved: (invoice, responseCode) => {
+            logger.success(`Invoice Regenerated ${invoice}`);
+        },
+        onRequestFailure: (error) => {
+            logger.error(`Failed To regenerate Invoice for transcation - ${transaction.id} error - ${error}`);
+        },
+    });
+    return res.redirect(`/resources/invoices/${req.params.invoice}`);
 });
 
 module.exports = router;

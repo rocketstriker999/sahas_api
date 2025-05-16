be574136e5715dbe9e26456b6f12297edb6225b2
nisarg <hammerbyte.nisarg@gmail.com>
2025-05-16 23:08:10 +0530

    regen invoice

--- routes/invoices.js
+++ routes/invoices.js
@@ -2,31 +2,36 @@ const libExpress = require("express");
 const { requestService } = require("../utils");
 const { getTransactionByInvoice } = require("../db/transactions");
 const logger = require("../libs/logger");
-const { getUserByTransactionId } = require("../db/users");
-const { getProductById } = require("../db/products");
 
 const router = libExpress.Router();
 
-//update user's details before purchase if user is missing primary details -2
-router.get("/regenerate/:invoice", async (req, res) => {
-    logger.info(`Regenerating Invoice ${req.params.invoice}`);
-    console.log("CALLELD");
-    const transaction = await getTransactionByInvoice(req.params.invoice);
+//update user's details before purchase if user is missing primary details -1
+router.get("/regenrate/:invoice", async (req, res) => {
+    if ((transaction = await getTransactionByInvoice(req.params.invoice))) {
+        logger.info(`Regenerating Invoice ${transaction.invoice}`);
 
-    requestService({
-        requestServiceName: process.env.SERVICE_MEDIA,
-        requestPath: "generate/invoice",
-        requestMethod: "POST",
-        requestPostBody: {
-            transaction,
-            user: await getUserByTransactionId(transaction.id),
-            product: await getProductById(transaction.product_id),
-            percent_sgst: process.env.SGST,
-            percent_cgst: process.env.CGST,
-        },
-    });
+        requestService({
+            requestServiceName: process.env.SERVICE_MEDIA,
+            requestPath: "generate/invoice",
+            requestMethod: "POST",
+            requestPostBody: {
+                transaction,
+                user: await getUserByTransactionId(transaction.id),
+                product: await getProductById(transaction.product_id),
+                percent_sgst: process.env.SGST,
+                percent_cgst: process.env.CGST,
+            },
+            onResponseReceieved: (invoice, responseCode) => {
+                return res.redirect(`/resources/invoices/${invoice}`);
+            },
+            onRequestFailure: (error) => {
+                logger.error(`Failed To regenerate Invoice for transcation - ${transaction.id} error - ${error}`);
+                return res.redirect(`/not-found`);
+            },
+        });
+    }
 
-    return res.redirect(`/resources/invoices/${req.params.invoice}`);
+    return res.status(400).json({ error: "Missing or Invalid Invoice" });
 });
 
 module.exports = router;

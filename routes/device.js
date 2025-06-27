6b072d5c2fe473627699bb11b489a3f3a6eca5ef
nisarg <hammerbyte.nisarg@gmail.com>
2025-06-27 21:36:54 +0530

    device creation

--- routes/device.js
+++ routes/device.js
@@ -3,13 +3,9 @@ const libExpress = require("express");
 const router = libExpress.Router();
 
 router.get("/create", async (req, res) => {
-    setTimeout(() => {
-        res.status(201).json({
-            device_id: `${Date.now()}_${Math.random()
-                .toString(36)
-                .substring(2, 15)}`,
-        });
-    }, 3000);
+    res.status(200).json({
+        device_id: `${Date.now()}_${Math.random().toString(36).substring(2, 15)}`,
+    });
 });
 
 module.exports = router;

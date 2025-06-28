076e12f77a0d8c54c363599f04e811fb8820ed23
nisarg <hammerbyte.nisarg@gmail.com>
2025-06-28 14:55:01 +0530

    dev deploy

--- middlewares/has_deviceId.js
+++ middlewares/has_deviceId.js
@@ -2,8 +2,9 @@ const logger = require("../libs/logger");
 
 module.exports = async (req, res, next) => {
     if (req.headers["Device-ID"]) {
-        next();
+        return next();
     }
 
+    logger.error("Request Denied - Missing Device ID");
     return res.status(400).json({ error: "Missing Device ID" });
 };

d9200063e4d7e47572ec3589cc1350f1b4e406e5
nisarg <hammerbyte.nisarg@gmail.com>
2025-06-28 23:33:12 +0530

    devices changes

--- db/devices.js
+++ db/devices.js
@@ -2,10 +2,9 @@ const { executeSQLQueryParameterized } = require("../libs/db");
 const logger = require("../libs/logger");
 
 function getDevicesByToken(token) {
-    return executeSQLQueryParameterized(
-        `SELECT USERS.id,USER_DEVICES.* FROM USERS INNER JOIN USER_DEVICES ON USERS.id=USER_DEVICES.user_id WHERE USERS.token=?`,
-        [token]
-    ).catch((error) => {
+    return executeSQLQueryParameterized(`SELECT USERS.id,DEVICES.* FROM USERS INNER JOIN DEVICES ON USERS.id=DEVICES.user_id WHERE USERS.token=?`, [
+        token,
+    ]).catch((error) => {
         logger.error(`executeSQLQueryParameterized: ${error}`);
         return [];
     });

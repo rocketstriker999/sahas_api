8bf62757df7451913fa067581a43c50fd1a5483a
nisarg <hammerbyte.nisarg@gmail.com>
2025-06-29 03:54:36 +0530

    device validation

--- middlewares/has_device_finger_print.js
+++ middlewares/has_device_finger_print.js
@@ -2,11 +2,6 @@ const { isDeviceKnown } = require("../db/devices");
 const logger = require("../libs/logger");
 
 module.exports = async (req, res, next) => {
-    logger.info("headers" + JSON.stringify(req.headers));
-    logger.info("device_finger_print" + req.headers?.device_finger_print);
-
-    logger.info(await isDeviceKnown(req.headers?.device_finger_print));
-
     if (req.headers?.device_finger_print && (isDeviceKnown = await isDeviceKnown(req.headers?.device_finger_print))) {
         return next();
     }
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const libCookieParser = require("cookie-parser");
 const logger = require("./libs/logger");
 const cors = require("cors");
 const requests = require("./middlewares/requests");
-const hasDevice = require("./middlewares/has_device");
+const hasDeviceFingerPrint = require("./middlewares/has_device_finger_print");
 const isDeviceAllowed = require("./middlewares/is_device_allowed");
 const hasAuthentication = require("./middlewares/has_authentication");
 
@@ -31,7 +31,7 @@ const routers = {
     "/otp": { middlewares: [], router: require("./routes/otp") },
     "/device": { middlewares: [], router: require("./routes/device") },
     "/transactions": { middlewares: [], router: require("./routes/transaction") },
-    "/media": { middlewares: [hasAuthentication, hasDevice, isDeviceAllowed], router: require("./routes/media") },
+    "/media": { middlewares: [hasAuthentication, hasDeviceFingerPrint, isDeviceAllowed], router: require("./routes/media") },
     "/extract": { middlewares: [], router: require("./routes/extract") },
     "/access": { middlewares: [], router: require("./routes/access") },
     "/catelogue": { middlewares: [], router: require("./routes/catelogue") },

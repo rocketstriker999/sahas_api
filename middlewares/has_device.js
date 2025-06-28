4030f34d51edd9d8c10337f43a3c34b221432d18
nisarg <hammerbyte.nisarg@gmail.com>
2025-06-29 03:42:56 +0530

    device validation

--- middlewares/has_device.js
+++ middlewares/has_device.js
@@ -2,6 +2,7 @@ const { isDeviceKnown } = require("../db/devices");
 const logger = require("../libs/logger");
 
 module.exports = async (req, res, next) => {
+    logger.info("headers" + req.headers);
     logger.info("device_finger_print" + req.headers?.device_finger_print);
 
     logger.info(await isDeviceKnown(req.headers?.device_finger_print));

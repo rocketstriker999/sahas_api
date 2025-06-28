129bad71385a5efc751a3f77f6d51f0ac23717bd
nisarg <hammerbyte.nisarg@gmail.com>
2025-06-28 23:28:36 +0530

    validation

--- routes/device.js
+++ routes/device.js
@@ -5,7 +5,7 @@ const router = libExpress.Router();
 
 //create a new device into datbase
 router.post("/create", async (req, res) => {
-    if (!req.headers.device_id && req.body.os && req.body.company && req.body.browser) {
+    if (!Boolean(req.headers.device_id) && req.body.os && req.body.company && req.body.browser) {
         return res.status(201).json({
             device_id: await addDevice(req.body),
         });

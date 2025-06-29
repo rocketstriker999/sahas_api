25951ba567f8b99b40ffa004c497d381a931aa04
nisarg <hammerbyte.nisarg@gmail.com>
2025-06-30 03:28:45 +0530

    deploy

--- middlewares/requires_user_association_with_device.js
+++ middlewares/requires_user_association_with_device.js
@@ -6,7 +6,7 @@ const DEVICE_FINGER_PRINT_KEY = "device-finger-print";
 module.exports = async (req, res, next) => {
     if (!req.device) {
         logger.error("Request Denied - Missing or Invalid Device FingerPrint");
-        return res.status(401).json({ error: "Missing Device FingerPrint" });
+        return res.status(401).json({ error: "Missing or Invalid Device FingerPrint" });
     }
 
     if (!req.device.isCurrentUserAssociatedWithDevice) {

8a4479b52f8977ed7f76beb1401afe5a5fbab94e
nisarg <hammerbyte.nisarg@gmail.com>
2025-07-06 23:28:22 +0530

    dev deploy

--- middlewares/parse_user.js
+++ middlewares/parse_user.js
@@ -3,6 +3,8 @@ const { getUserByToken } = require("../db/users");
 
 module.exports = async (req, res, next) => {
     //verify token and get user information
+    logger.info("PARSEUSER--");
+
     if (req.headers?.[KEY_AUTHENTICATION_TOKEN] && (user = await getUserByToken(req.headers?.[KEY_AUTHENTICATION_TOKEN]))) {
         req.user = user;
     }

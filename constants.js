51b32adbd168d9c1e0d8e11bcb0fb33d0feec319
nisarg <hammerbyte.nisarg@gmail.com>
2025-07-06 18:29:11 +0530

    device key

--- constants.js
+++ constants.js
@@ -2,6 +2,6 @@ module.exports = {
     REQUEST_DENIED: "Request Denied",
     SERVER_UNDER_MAINTENANCE: "Server Under Maintenance",
     ROUTE_NOT_FOUND: "Route Not Found",
-    KEY_DEVICE_FINGER_PRINT: "device-finger-print",
+    KEY_DEVICE_FINGER_PRINT: "device_finger_print",
     KEY_AUTHENTICATION_TOKEN: "auth_token",
 };

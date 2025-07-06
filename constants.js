beca62d778f04642d3462eced8261ec099f0ee54
nisarg <hammerbyte.nisarg@gmail.com>
2025-07-06 23:19:38 +0530

    headers placing

--- constants.js
+++ constants.js
@@ -2,8 +2,8 @@ module.exports = {
     REQUEST_DENIED: "Request Denied",
     SERVER_UNDER_MAINTENANCE: "Server Under Maintenance",
     ROUTE_NOT_FOUND: "Route Not Found",
-    KEY_DEVICE_FINGER_PRINT: "device_finger_print",
-    KEY_DEVICE_DESCRIPTION: "device_description",
+    KEY_DEVICE_FINGER_PRINT: "Device-Finger-Print",
+    KEY_DEVICE_DESCRIPTION: "Device-Description",
     KEY_AUTHENTICATION_TOKEN: "auth_token",
     ERROR_DEVICE_MISSING: "Device Information Is Missing",
 };

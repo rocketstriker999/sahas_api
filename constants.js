24e0a6d41d070526bda5463550e9bb027be41169
nisarg <hammerbyte.nisarg@gmail.com>
2025-07-06 23:24:29 +0530

    dev deploy

--- constants.js
+++ constants.js
@@ -2,8 +2,8 @@ module.exports = {
     REQUEST_DENIED: "Request Denied",
     SERVER_UNDER_MAINTENANCE: "Server Under Maintenance",
     ROUTE_NOT_FOUND: "Route Not Found",
-    KEY_DEVICE_FINGER_PRINT: "Device-Finger-Print",
-    KEY_DEVICE_DESCRIPTION: "Device-Description",
+    KEY_DEVICE_FINGER_PRINT: "device-finger-print",
+    KEY_DEVICE_DESCRIPTION: "device-description",
     KEY_AUTHENTICATION_TOKEN: "auth_token",
     ERROR_DEVICE_MISSING: "Device Information Is Missing",
 };

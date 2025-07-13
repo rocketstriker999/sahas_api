5c2fc7f240588fe6f9fb3ef709ce45d7ac2cf859
nisarg <hammerbyte.nisarg@gmail.com>
2025-07-14 01:49:20 +0530

    dev deploy

--- db/authentication_tokens.js
+++ db/authentication_tokens.js
@@ -6,12 +6,15 @@ async function addInactiveToken(userId, token, otp) {
     //get allowed validity from configuration
     const { authentication: { validity } = {} } = await readConfig("app");
 
-    return executeSQLQueryParameterized(`INSERT INTO USER_TOKENS(user_id,otp,token,validity)`, [userId, token, otp, validity * 24 * 60 * 60 * 1000]).catch(
-        (error) => {
-            logger.error(`addInactiveToken: ${error}`);
-            return [];
-        }
-    );
+    return executeSQLQueryParameterized(`INSERT INTO USER_TOKENS(user_id,otp,token,validity) VALUES(?,?,?,?)`, [
+        userId,
+        token,
+        otp,
+        validity * 24 * 60 * 60 * 1000,
+    ]).catch((error) => {
+        logger.error(`addInactiveToken: ${error}`);
+        return [];
+    });
 }
 
 module.exports = { addInactiveToken };

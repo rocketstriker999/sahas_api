62a6ab2454506e970eb2b6aadb61429bae3cb042
nisarg <hammerbyte.nisarg@gmail.com>
2025-07-29 05:28:07 +0530

    dev deploy

--- routes/catelogue.js
+++ routes/catelogue.js
@@ -6,22 +6,9 @@ const router = libExpress.Router();
 
 //get catelogue for user
 router.get("/", async (req, res) => {
-    //get user's accesses
-    const userAccesses = req?.user
-        ? Object.assign({}, ...(await getAccessesByToken(req.headers?.[KEY_AUTHENTICATION_TOKEN]))?.map((access) => ({ [access.product_id]: access.invoice })))
-        : [];
-
     //prepare and return catelogue
     res.status(200).json({
-        categories: cacher.get(process.env.CACHE_KEYS_CATEGORIES),
-
-        products: cacher.get(process.env.CACHE_KEYS_PRODUCTS)?.map((product) => ({
-            ...product,
-            ...(Object.keys(userAccesses)?.includes(product.id.toString()) && { has_access: true, invoice: userAccesses[product.id.toString()] }),
-        })),
-        courses: cacher.get(process.env.CACHE_KEYS_COURSES),
-        subjects: cacher.get(process.env.CACHE_KEYS_SUBJECTS),
-        chapters: cacher.get(process.env.CACHE_KEYS_CHAPTERS),
+        data: "wad",
     });
 });
 

79d166ecfe767f5508fb7c6ec31eed0b3d4aac7f
nisarg <hammerbyte.nisarg@gmail.com>
2025-07-29 04:52:40 +0530

    dev deploy

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ sahasAPI.use(cors({ origin: process.env.ALLOWED_CORS_ORIGINS }));
 sahasAPI.use(libExpress.json());
 sahasAPI.use(libExpress.urlencoded({ extended: true }));
 
-//Apply Middlewares #5
+//Apply Middlewares #6
 sahasAPI.use((req, res, next) => setTimeout(next, 3000));
 
 sahasAPI.use(requiresNoMaintenance);

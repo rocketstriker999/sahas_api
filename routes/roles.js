488d356ad6411668a752b9935f7625b522e3895f
nisarg <hammerbyte.nisarg@gmail.com>
2025-08-01 00:52:54 +0530

    dev deploy

--- routes/roles.js
+++ routes/roles.js
@@ -1,11 +1,11 @@
 const libExpress = require("express");
 const logger = require("../libs/logger");
-const { route } = require("./data_dump");
 const { getRoles } = require("../db/roles");
 
 const router = libExpress.Router();
 
 //get all roles
+
 router.get("/", async (req, res) => {
     res.status(200).json(await getRoles());
 });

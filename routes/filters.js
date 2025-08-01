1e49c680c8a4e42c72b34792219135514446fefd
nisarg <hammerbyte.nisarg@gmail.com>
2025-08-02 01:39:26 +0530

    dev deploy

--- routes/filters.js
+++ routes/filters.js
@@ -11,7 +11,7 @@ router.get("/", async (req, res) => {
         roles: await getRoles(),
         branches: [{ title: "B1" }, { title: "B2" }],
         courses: [],
-        active: [true, false],
+        active: [{ title: "Yes" }, { title: "No" }],
     });
 });
 

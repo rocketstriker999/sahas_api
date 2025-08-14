bbab78b7704c6691dd5e0222b60a45829253c20b
nisarg <hammerbyte.nisarg@gmail.com>
2025-08-15 02:56:18 +0530

    dev deploy

--- db/inquiries.js
+++ db/inquiries.js
@@ -2,7 +2,10 @@ const { executeSQLQueryParameterized } = require("../libs/db");
 const logger = require("../libs/logger");
 
 function getInquiriesByUserId(userId) {
-    return executeSQLQueryParameterized("SELECT * FROM USER_INQUIRIES WHERE user_id=?", [userId]).catch((error) => {
+    return executeSQLQueryParameterized(
+        "SELECT USER_INQUIRIES.*,USERS.full_name as created_by_full_name FROM USER_INQUIRIES LEFT JOIN USERS ON USER_INQUIRIES.created_by=USERS.id WHERE USER_INQUIRIES.user_id=?",
+        [userId]
+    ).catch((error) => {
         logger.error(`getInquiriesByUserId: ${error}`);
         return [];
     });

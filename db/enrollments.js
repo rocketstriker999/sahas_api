5cafd224d788c18b1697d120d329380a355041bc
nisarg <hammerbyte.nisarg@gmail.com>
2025-08-17 02:12:25 +0530

    dev deploy

--- db/enrollments.js
+++ db/enrollments.js
@@ -2,7 +2,10 @@ const { executeSQLQueryParameterized } = require("../libs/db");
 const logger = require("../libs/logger");
 
 function getEnrollmentsByUserId(userId) {
-    return executeSQLQueryParameterized("SELECT * FROM USER_ENROLLMENTS WHERE user_id=? ORDER BY id DESC", [userId]).catch((error) => {
+    return executeSQLQueryParameterized(
+        "SELECT id,user_id,DATE(start_date),DATE(end_date),total,active,created_on,updated_at FROM USER_ENROLLMENTS WHERE user_id=? ORDER BY id DESC",
+        [userId]
+    ).catch((error) => {
         logger.error(`getEnrollmentsByUserId: ${error}`);
         return [];
     });

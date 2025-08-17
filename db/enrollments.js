19b1057bd30701f090e97643706d03523f3e18bf
nisarg <hammerbyte.nisarg@gmail.com>
2025-08-17 18:41:51 +0530

    dev deploy

--- db/enrollments.js
+++ db/enrollments.js
@@ -2,10 +2,7 @@ const { executeSQLQueryParameterized } = require("../libs/db");
 const logger = require("../libs/logger");
 
 function getEnrollmentsByUserId(userId) {
-    return executeSQLQueryParameterized(
-        "SELECT id,user_id,DATE(start_date) as start_date,DATE(end_date) as end_date,total,active,created_on,updated_at FROM USER_ENROLLMENTS WHERE user_id=? ORDER BY id DESC",
-        [userId]
-    ).catch((error) => {
+    return executeSQLQueryParameterized("SELECT * FROM USER_ENROLLMENTS WHERE user_id=? ORDER BY id DESC", [userId]).catch((error) => {
         logger.error(`getEnrollmentsByUserId: ${error}`);
         return [];
     });
@@ -21,10 +18,7 @@ function updateEnrollmentByEnrollmentId({ id, active, start_date, end_date }) {
 }
 
 function getEnrollmentByEnrollmentId(enrollmentId) {
-    return executeSQLQueryParameterized(
-        "SELECT id,user_id,DATE(start_date) as start_date,DATE(end_date) as end_date,total,active,created_on,updated_at FROM USER_ENROLLMENTS WHERE id =?",
-        [enrollmentId]
-    )
+    return executeSQLQueryParameterized("SELECT * FROM USER_ENROLLMENTS WHERE id =?", [enrollmentId])
         .then((results) => {
             return results.length > 0 ? results[0] : null;
         })

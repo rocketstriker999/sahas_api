57e897b62dc2f2a5ec1cbdecd9b361af953457bc
nisarg <hammerbyte.nisarg@gmail.com>
2025-08-19 00:47:56 +0530

    dev deploy

--- routes/configs.js
+++ routes/configs.js
@@ -1,5 +1,5 @@
 const libExpress = require("express");
-const { readConfig, getConfigs } = require("../libs/config");
+const { readConfig } = require("../libs/config");
 const logger = require("../libs/logger");
 const { getAllBranches } = require("../db/branches");
 const { getAllCourses } = require("../db/courses");
@@ -15,7 +15,7 @@ router.get("/template", async (req, res) => {
         config = await readConfig(req.params.config);
         config.global.branches = await getAllBranches();
         config.global.courses = await getAllCourses();
-        config.global.paymentTypes = payment.types;
+        config.global.paymentTypes = payment?.types;
     } catch (error) {
         logger.error(error);
     } finally {

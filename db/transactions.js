3d6a530eb0f3f1963ae8927f6743967b3a45019e
nisarg <hammerbyte.nisarg@gmail.com>
2025-08-18 23:03:27 +0530

    added full name

--- db/transactions.js
+++ db/transactions.js
@@ -146,7 +146,10 @@ const logger = require("../libs/logger");
 // }
 
 function getTransactionsByEnrollmentId(enrollmentId) {
-    return executeSQLQueryParameterized(`SELECT * FROM ENROLLMENT_TRANSACTIONS WHERE enrollment_id=?`, [enrollmentId]).catch((error) => {
+    return executeSQLQueryParameterized(
+        `SELECT ENROLLMENT_TRANSACTIONS.*,USERS.full_name AS created_by_full_name FROM ENROLLMENT_TRANSACTIONS LEFT JOIN USERS ON ENROLLMENT_TRANSACTIONS.created_by=USERS.id WHERE enrollment_id=?`,
+        [enrollmentId]
+    ).catch((error) => {
         logger.error(`getTransactionsByEnrollmentId: ${error}`);
         return [];
     });

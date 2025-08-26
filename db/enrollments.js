eee5e923a38380961d34b58a326e965bccfef877
nisarg <hammerbyte.nisarg@gmail.com>
2025-08-27 04:28:09 +0530

    dev deploy

--- db/enrollments.js
+++ db/enrollments.js
@@ -4,7 +4,7 @@ const logger = require("../libs/logger");
 //freeze
 function getEnrollmentsByUserId({ user_id }) {
     return executeSQLQueryParameterized(
-        "SELECT ENROLLMENTS.*,USERS.full_name AS created_by_full_name FROM ENROLLMENTS LEFT JOIN USERS ON ENROLLMENTS.created_by=USERS.id WHERE user_id=? ORDER BY id DESC",
+        "SELECT ENROLLMENTS.*,USERS.full_name AS created_by_full_name FROM ENROLLMENTS LEFT JOIN USERS ON ENROLLMENTS.created_by=USERS.id WHERE ENROLLMENTS.user_id=? ORDER BY id DESC",
         [user_id]
     ).catch((error) => {
         logger.error(`getEnrollmentsByUserId: ${error}`);
@@ -22,11 +22,11 @@ function updateEnrollmentById({ id, active, start_date, end_date }) {
 //freeze
 function getEnrollmentById({ id }) {
     return executeSQLQueryParameterized(
-        "SELECT ENROLLMENTS.*,USERS.full_name AS created_by_full_name FROM ENROLLMENTS LEFT JOIN USERS ON ENROLLMENTS.created_by=USERS.id WHERE id=?",
+        "SELECT ENROLLMENTS.*,USERS.full_name AS created_by_full_name FROM ENROLLMENTS LEFT JOIN USERS ON ENROLLMENTS.created_by=USERS.id WHERE ENROLLMENTS.id=?",
         [id]
     )
         .then((results) => (results.length > 0 ? results[0] : null))
-        .catch((error) => logger.error(`getEnrollmentByEnrollmentId: ${error}`));
+        .catch((error) => logger.error(`getEnrollmentById: ${error}`));
 }
 
 function addEnrollment({ user_id, start_date, end_date, fees, created_by }) {

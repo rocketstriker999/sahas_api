147e74795e8b5d8ca49f1112dae5ec4e588a13f4
nisarg <hammerbyte.nisarg@gmail.com>
2025-08-28 18:10:06 +0530

    dev deploy

--- db/enrollment_courses.js
+++ db/enrollment_courses.js
@@ -1,6 +1,7 @@
 const { executeSQLQueryParameterized, executeSQLQueryRaw } = require("../libs/db");
 const logger = require("../libs/logger");
 
+//freeze
 function getEnrollmentCoursesByEnrollmentId({ enrollment_id }) {
     return executeSQLQueryParameterized(
         `SELECT ENROLLMENT_COURSES.*,USERS.full_name AS created_by_full_name FROM ENROLLMENT_COURSES LEFT JOIN USERS ON ENROLLMENT_COURSES.created_by=USERS.id WHERE ENROLLMENT_COURSES.enrollment_id=? ORDER BY ENROLLMENT_COURSES.id DESC`,

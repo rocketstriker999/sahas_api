9330aa4c219a6e527ced0135217a3f79bf8701b0
nisarg <hammerbyte.nisarg@gmail.com>
2025-09-08 01:47:54 +0530

    dev deploy

--- db/product_categories.js
+++ db/product_categories.js
@@ -9,13 +9,14 @@ function getAllproductCategories() {
 
 function getProductCategoryById({ id }) {
     return executeSQLQueryParameterized(
-        `SELECT PRODUCT_CATEGORIES.*, COUNT(PRODUCTS.id) AS products_count FROM PRODUCT_CATEGORIES LEFT JOIN PRODUCTS ON PRODUCTS.category = PRODUCT_CATEGORIES.id GROUP BY PRODUCT_CATEGORIES.id WHERE id=? ORDER BY PRODUCT_CATEGORIES.view_index`,
+        `SELECT PRODUCT_CATEGORIES.*, COUNT(PRODUCTS.id) AS products_count FROM PRODUCT_CATEGORIES LEFT JOIN PRODUCTS ON PRODUCTS.category = PRODUCT_CATEGORIES.id WHERE PRODUCT_CATEGORIES.id = ? GROUP BY PRODUCT_CATEGORIES.id ORDER BY PRODUCT_CATEGORIES.view_index`,
         [id]
     )
         .then((result) => (result.length > 0 ? result[0] : false))
-        .catch((error) => logger.error(`getAllproductCategories: ${error}`));
+        .catch((error) => logger.error(`getProductCategoryById: ${error}`));
 }
 
+//freeze
 function addProductCategory({ title, image }) {
     return executeSQLQueryParameterized(`INSERT INTO PRODUCT_CATEGORIES(title,image) VALUES(?,?)`, [title, image]).catch((error) =>
         logger.error(`addProductCategory: ${error}`)

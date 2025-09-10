ef0cdeb0e6473dea4602b96946286de0707818cc
nisarg <hammerbyte.nisarg@gmail.com>
2025-09-11 03:21:02 +0530

    node app

--- routes/media.js
+++ routes/media.js
@@ -6,28 +6,28 @@ const requiresUserDeviceActiveMapping = require("../middlewares/check_device");
 const router = libExpress.Router();
 
 //request for demo content from subjectid
-router.get("/subjects/:subjectId", async (req, res) => {
-    if (req.params.subjectId) {
-        const content = await getMediaBySubjectId(req.params.subjectId);
-        return res.status(200).json(content);
-    }
-    return res.status(400).json({ error: "Missing Content subjectId" });
-});
+// router.get("/subjects/:subjectId", async (req, res) => {
+//     if (req.params.subjectId) {
+//         const content = await getMediaBySubjectId(req.params.subjectId);
+//         return res.status(200).json(content);
+//     }
+//     return res.status(400).json({ error: "Missing Content subjectId" });
+// });
 
-//request for content from chapterId
-router.get("/chapters/:chapterId", requiresUserDeviceActiveMapping, async (req, res) => {
-    if (req.params.chapterId) {
-        //cehck if this user has access to course
-        if (await verifyAccessByTokenForChapter(req.cookies.token, req.params.chapterId)) {
-            //check if this device has access
+// //request for content from chapterId
+// router.get("/chapters/:chapterId", requiresUserDeviceActiveMapping, async (req, res) => {
+//     if (req.params.chapterId) {
+//         //cehck if this user has access to course
+//         if (await verifyAccessByTokenForChapter(req.cookies.token, req.params.chapterId)) {
+//             //check if this device has access
 
-            const content = await getMediaByChapterId(req.params.chapterId);
-            return res.status(200).json(content);
-        }
-        return res.status(401).json({ error: "You Don't Have access to this content" });
-    }
-    return res.status(400).json({ error: "Missing Required Details" });
-});
+//             const content = await getMediaByChapterId(req.params.chapterId);
+//             return res.status(200).json(content);
+//         }
+//         return res.status(401).json({ error: "You Don't Have access to this content" });
+//     }
+//     return res.status(400).json({ error: "Missing Required Details" });
+// });
 
 router.post("/image", libExpress.raw({ type: "image/*", limit: "10mb" }), (req, res) => {
     try {

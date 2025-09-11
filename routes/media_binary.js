db397205331e67ee2ed2063da4e28b4fa6ee3dcd
nisarg <hammerbyte.nisarg@gmail.com>
2025-09-11 21:28:37 +0530

    dev deploy

--- routes/media_binary.js
+++ routes/media_binary.js
@@ -3,17 +3,21 @@ const router = libExpress.Router();
 const libMulter = require("multer");
 const libPath = require("path");
 
-const storage = libMulter.diskStorage({
-    destination: (req, file, next) => {
-        next(null, process.env.DIRECTORY_UPLOAD_BINARIES);
-    },
-    filename: (req, file, next) => {
-        next(null, `${Date.now()}${libPath.extname(file.originalname)}`);
-    },
-});
-
-router.post("/", libMulter({ storage }).single("file"), (req, res) => {
-    res.status(201).json({ success: true, file: req.file });
-});
+router.post(
+    "/",
+    libMulter({
+        storage: libMulter.diskStorage({
+            destination: (req, file, next) => {
+                next(null, process.env.DIRECTORY_UPLOAD_BINARIES);
+            },
+            filename: (req, file, next) => {
+                next(null, `${Date.now()}${libPath.extname(file.originalname)}`);
+            },
+        }),
+    }).single("file"),
+    (req, res) => {
+        res.status(201).json({ success: true, file: req.file });
+    }
+);
 
 module.exports = router;

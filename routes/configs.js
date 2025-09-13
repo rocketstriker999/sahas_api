c682af5d54a1eb26f1023eab6c93b95fe78aaa6a
nisarg <hammerbyte.nisarg@gmail.com>
2025-09-14 00:32:39 +0530

    dev deploy

--- routes/configs.js
+++ routes/configs.js
@@ -27,21 +27,19 @@ router.get("/template", async (req, res) => {
 router.patch("/template/dashboard/carousel_images", async (req, res) => {
     const requiredBodyFields = ["click_link", "source"];
 
-    const { isRequestBodyValid, missingRequestBodyFields, validatedRequestBody } = validateRequestBody(req.body, requiredBodyFields);
-
-    if (isRequestBodyValid) {
-        try {
+    try {
+        const { isRequestBodyValid, missingRequestBodyFields, validatedRequestBody } = validateRequestBody(req.body, requiredBodyFields);
+        if (isRequestBodyValid) {
             const config = await readConfig("template");
             config.dashboard.carousel_images = [...config.dashboard.carousel_images, validatedRequestBody];
             writeConfig("template", config);
-        } catch (error) {
-            logger.error(error);
-            res.status(400).json({ error });
+            res.sendStatus(201);
+        } else {
+            throw new Error(`Missing ${missingRequestBodyFields?.join(",")}`);
         }
-
-        res.sendStatus(201);
-    } else {
-        res.status(400).json({ error: `Missing ${missingRequestBodyFields?.join(",")}` });
+    } catch (error) {
+        logger.error(error);
+        res.status(400).json({ error });
     }
 });
 

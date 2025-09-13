205ce472e7b86edd31a6111ac09deb32dbced39f
nisarg <hammerbyte.nisarg@gmail.com>
2025-09-14 03:31:52 +0530

    dev deploy

--- libs/config.js
+++ libs/config.js
@@ -5,13 +5,18 @@ const libPath = require("path");
 // write configuration and save
 function writeConfig(configName, configuration) {
     return new Promise((resolve, reject) => {
-        libFs.writeFile(libPath.join(process.cwd(), process.env.DIR_CONFIGS, configName), JSON.stringify(configuration, null, 2), "utf8", (error) => {
-            if (error) {
-                reject(error);
-            } else {
-                resolve();
+        libFs.writeFile(
+            libPath.join(process.cwd(), process.env.DIR_CONFIGS, configName.concat(".json")),
+            JSON.stringify(configuration, null, 2),
+            "utf8",
+            (error) => {
+                if (error) {
+                    reject(error);
+                } else {
+                    resolve();
+                }
             }
-        });
+        );
     });
 }
 

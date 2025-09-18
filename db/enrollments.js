cfd8bdfed106c479929708dd1a49ec3cfa1da911
nisarg <hammerbyte.nisarg@gmail.com>
2025-09-19 00:57:43 +0530

    dev deploy

--- db/enrollments.js
+++ db/enrollments.js
@@ -30,12 +30,13 @@ function getEnrollmentById({ id }) {
 }
 
 //freeze
-function addEnrollment({ user_id, start_date, end_date, fees, created_by }) {
-    return executeSQLQueryParameterized("INSERT INTO ENROLLMENTS(user_id,start_date,end_date,fees,created_by) VALUES(?,?,?,?,?)", [
+function addEnrollment({ user_id, start_date, end_date, fees, on_site_access, created_by }) {
+    return executeSQLQueryParameterized("INSERT INTO ENROLLMENTS(user_id,start_date,end_date,fees,on_site_access,created_by) VALUES(?,?,?,?,?,?)", [
         user_id,
         start_date,
         end_date,
         fees,
+        on_site_access,
         created_by,
     ])
         .then((result) => result.insertId)

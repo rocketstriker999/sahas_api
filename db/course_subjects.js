10014a1b5739b6c8b69ea1b16d574cd7c024abf1
nisarg <hammerbyte.nisarg@gmail.com>
2025-10-05 02:19:39 +0530

    subject_id added

--- db/course_subjects.js
+++ db/course_subjects.js
@@ -4,7 +4,7 @@ const logger = require("../libs/logger");
 //freeze
 function getCourseSubjectsByCourseId({ course_id }) {
     return executeSQLQueryParameterized(
-        `SELECT COURSE_SUBJECTS.id,SUBJECTS.title,SUBJECTS.active,SUBJECTS.updated_at FROM COURSE_SUBJECTS LEFT JOIN SUBJECTS ON COURSE_SUBJECTS.subject_id=SUBJECTS.id WHERE COURSE_SUBJECTS.course_id=? ORDER BY COURSE_SUBJECTS.view_index ASC`,
+        `SELECT COURSE_SUBJECTS.id,SUBJECTS.id AS subject_id,SUBJECTS.title,SUBJECTS.active,SUBJECTS.updated_at FROM COURSE_SUBJECTS LEFT JOIN SUBJECTS ON COURSE_SUBJECTS.subject_id=SUBJECTS.id WHERE COURSE_SUBJECTS.course_id=? ORDER BY COURSE_SUBJECTS.view_index ASC`,
         [course_id]
     ).catch((error) => {
         logger.error(`getCourseSubjectsByCourseId: ${error}`);

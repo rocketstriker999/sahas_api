0be494ae3ce1121c3dda4f61ac559c108eef5933
nisarg <hammerbyte.nisarg@gmail.com>
2025-10-04 19:50:04 +0530

    import fix

--- routes/subjects.js
+++ routes/subjects.js
@@ -1,8 +1,5 @@
 const libExpress = require("express");
-const { addCourse, getCourseById, deleteCourseById, updateCourseViewIndexById, updateCourse, updateCourseById } = require("../db/courses");
-const { validateRequestBody } = require("../utils");
-const { getEnrollmentByCourseIdAndUserId } = require("../db/enrollments");
-const { getCourseSubjects, updateCoursesSubjectsViewIndexById } = require("../db/courses_subjects");
+
 const { updateSubjectViewIndexById } = require("../db/subjects");
 
 const router = libExpress.Router();

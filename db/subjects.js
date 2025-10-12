86735628f22ba735a63ab7b1403f0cff5a580cf7
nisarg <hammerbyte.nisarg@gmail.com>
2025-10-12 22:49:36 +0530

    bg color fix

--- db/subjects.js
+++ db/subjects.js
@@ -4,8 +4,8 @@ const logger = require("../libs/logger");
 //freeze
 function addSubject({ title, background_color }) {
     return executeSQLQueryParameterized(`INSERT INTO SUBJECTS(title,background_color) VALUES(?,?)`, [title, background_color])
-        .then((result) => result.insertId)
-        .catch((error) => logger.error(`addRole: ${error}`));
+        .then(({ insertId }) => insertId)
+        .catch((error) => logger.error(`addSubject: ${error}`));
 }
 
 module.exports = { addSubject };

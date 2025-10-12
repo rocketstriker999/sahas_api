ee31ebb1e7d7e0a3d7046632e73af9a11ca11eff
nisarg <hammerbyte.nisarg@gmail.com>
2025-10-12 22:51:53 +0530

    debug subjects add

--- db/subjects.js
+++ db/subjects.js
@@ -3,8 +3,9 @@ const logger = require("../libs/logger");
 
 //freeze
 function addSubject({ title, background_color }) {
+    logger.info(`INSERT INTO SUBJECTS(title,background_color) VALUES(${title},${background_color})`);
     return executeSQLQueryParameterized(`INSERT INTO SUBJECTS(title,background_color) VALUES(?,?)`, [title, background_color])
-        .then(({ insertId }) => insertId)
+        .then((result) => result.insertId)
         .catch((error) => logger.error(`addSubject: ${error}`));
 }
 

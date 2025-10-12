0903f076e83e504ed209210e1ceb123536c8d625
nisarg <hammerbyte.nisarg@gmail.com>
2025-10-12 23:07:22 +0530

    option field support

--- db/subjects.js
+++ db/subjects.js
@@ -3,7 +3,6 @@ const logger = require("../libs/logger");
 
 //freeze
 function addSubject({ title, background_color }) {
-    logger.info(`INSERT INTO SUBJECTS(title,background_color) VALUES(${title},${background_color})`);
     return executeSQLQueryParameterized(`INSERT INTO SUBJECTS(title,background_color) VALUES(?,?)`, [title, background_color])
         .then((result) => result.insertId)
         .catch((error) => logger.error(`addSubject: ${error}`));

4bf7bdf11fb19d6b0c59b1cb795ec5591c11aaa2
nisarg <hammerbyte.nisarg@gmail.com>
2025-10-22 03:33:07 +0530

    query fix

--- db/coupon_code_courses.js
+++ db/coupon_code_courses.js
@@ -3,7 +3,7 @@ const logger = require("../libs/logger");
 
 function getCouponCodeCoursesByCouponCodeId({ coupon_code_id }) {
     return executeSQLQueryParameterized(
-        `SELECT COUPON_CODE_COURSES.*,COURSES.title,USERS.full_name as distributor_name FROM COUPON_CODE_COURSES LEFT JOIN COURSES ON COUPON_CODE_COURSES.course_id=COURSES.id  LEFT JOIN ON USERS WHERE COUPON_CODE_COURSES.email=USERS.email WHERE COUPON_CODE_COURSES.coupon_code_id=?`,
+        `SELECT COUPON_CODE_COURSES.*,COURSES.title,USERS.full_name as distributor_name FROM COUPON_CODE_COURSES LEFT JOIN COURSES ON COUPON_CODE_COURSES.course_id=COURSES.id  LEFT JOIN ON USERS ON COUPON_CODE_COURSES.email=USERS.email WHERE COUPON_CODE_COURSES.coupon_code_id=?`,
         [coupon_code_id]
     ).catch((error) => {
         logger.error(`getAllCouponCodes: ${error}`);
@@ -24,7 +24,7 @@ function addCouponCodeCourse({ coupon_code_id, course_id, discount, discount_typ
 //freeze - need to optimizae as well
 function getCouponCodeCoursesByIds({ couponCodeCourseIds }) {
     return executeSQLQueryParameterized(
-        `SELECT COUPON_CODE_COURSES.*,COURSES.title,USERS.full_name as distributor_name FROM COUPON_CODE_COURSES LEFT JOIN COURSES ON COUPON_CODE_COURSES.course_id=COURSES.id LEFT JOIN ON USERS WHERE COUPON_CODE_COURSES.email=USERS.email WHERE COUPON_CODE_COURSES.id in (${couponCodeCourseIds})`
+        `SELECT COUPON_CODE_COURSES.*,COURSES.title,USERS.full_name as distributor_name FROM COUPON_CODE_COURSES LEFT JOIN COURSES ON COUPON_CODE_COURSES.course_id=COURSES.id LEFT JOIN ON USERS ON COUPON_CODE_COURSES.email=USERS.email WHERE COUPON_CODE_COURSES.id in (${couponCodeCourseIds})`
     ).catch((error) => {
         logger.error(`getCouponCodeCoursesByIds: ${error}`);
         return [];

feb81ab0ace9b54e7fe5b1e883c27fc665eec825
nisarg <hammerbyte.nisarg@gmail.com>
2025-10-30 19:57:37 +0530

    cache fix

--- libs/cacher.js
+++ libs/cacher.js
@@ -1,6 +1,7 @@
 const { CACHE_KEY_COURSE_CATEGORIES, CACHE_KEY_COURSES, CACHE_KEY_SUBJECTS, CACHE_KEY_PAYMENT_GATEWAY_PAYLOADS } = require("../constants");
 const { getAllCourseCategories } = require("../db/course_categories");
 const { getAllCourses } = require("../db/courses");
+const { getAllPaymentGateWayPayLoads } = require("../db/payment_gateway_payloads");
 const { getAllSubjects } = require("../db/subjects");
 const logger = require("./logger");
 
@@ -24,7 +25,7 @@ const generateCaches = async () => {
     await add(CACHE_KEY_COURSE_CATEGORIES, getAllCourseCategories);
     await add(CACHE_KEY_COURSES, getAllCourses);
     await add(CACHE_KEY_SUBJECTS, getAllSubjects);
-    await add(CACHE_KEY_PAYMENT_GATEWAY_PAYLOADS, getAllSubjects);
+    await add(CACHE_KEY_PAYMENT_GATEWAY_PAYLOADS, getAllPaymentGateWayPayLoads);
 };
 
 module.exports = { add, get, refresh, generateCaches };

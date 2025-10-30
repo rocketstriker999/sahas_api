ff2fcad1e9b787cd3e3e163a036e8d7ca67cec42
nisarg <hammerbyte.nisarg@gmail.com>
2025-10-30 19:49:25 +0530

    transcation

--- routes/payment_gateway_payload_results.js
+++ routes/payment_gateway_payload_results.js
@@ -2,11 +2,13 @@ const libExpress = require("express");
 const { validateRequestBody } = require("../utils");
 const logger = require("../libs/logger");
 const { readConfig } = require("../libs/config");
+const { get } = require("../libs/cacher");
+const { KEY_AUTHENTICATION_TOKEN } = require("../constants");
 
 const router = libExpress.Router();
 
 router.post("/", async (req, res) => {
-    const requiredBodyFields = ["status"];
+    const requiredBodyFields = ["status", "txnid"];
 
     const { isRequestBodyValid, missingRequestBodyFields, validatedRequestBody } = validateRequestBody(req.body, requiredBodyFields);
 
@@ -14,9 +16,10 @@ router.post("/", async (req, res) => {
 
     //verify into payu if payment is success
 
-    logger.info(JSON.stringify(validatedRequestBody.txnid));
-
     if (isRequestBodyValid) {
+        const transaction = get(CACHE_KEY_PAYMENT_GATEWAY_PAYLOADS)?.find(({ id }) => id == validatedRequestBody.txnid);
+        logger.info(transaction);
+
         res.redirect(redirectionHost);
     } else {
         res.status(400).json({ error: `Missing ${missingRequestBodyFields?.join(",")}` });

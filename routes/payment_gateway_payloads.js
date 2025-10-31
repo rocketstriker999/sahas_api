b2e323924286b4a1cd47da5267353b52630f09a3
nisarg <hammerbyte.nisarg@gmail.com>
2025-10-31 18:00:38 +0530

    dev deploy

--- routes/payment_gateway_payloads.js
+++ routes/payment_gateway_payloads.js
@@ -53,7 +53,7 @@ router.post("/", async (req, res) => {
                 if (couponCodeCourse?.discount_type === "%") {
                     paymentGateWayPayLoad.transaction.discount = (paymentGateWayPayLoad.transaction.amount * couponCodeCourse.discount) / 100;
                 }
-                paymentGateWayPayLoad.transaction.discount = Number(paymentGateWayPayLoad.transaction.discount);
+                // paymentGateWayPayLoad.transaction.discount = Number(paymentGateWayPayLoad.transaction.discount);
                 logger.info(paymentGateWayPayLoad.transaction.discount.toFixed(2));
             } else {
                 paymentGateWayPayLoad.transaction.discount = 0;

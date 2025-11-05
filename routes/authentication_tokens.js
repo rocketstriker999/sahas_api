485fcaac64c5292b60fc3723418f66aa13ac1510
nisarg <hammerbyte.nisarg@gmail.com>
2025-11-06 03:22:49 +0530

    tiny enhancmeent

--- routes/authentication_tokens.js
+++ routes/authentication_tokens.js
@@ -68,7 +68,6 @@ router.post("/", async (req, res) => {
         requestServiceName: process.env.SERVICE_MAILER,
         onRequestStart: () => logger.info("Generating OTP"),
         requestMethod: "POST",
-        requestPath: "",
         parseResponseBody: false,
         requestPostBody: {
             from: EMAIL_NO_REPLY,

9bae01d540948d40ccb14884addcebce2e3e1857
nisarg <hammerbyte.nisarg@gmail.com>
2025-11-15 03:52:48 +0530

    dev deploy

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const { logger } = require("sahas_utils");
 const cors = require("cors");
 const { ROUTE_NOT_FOUND } = require("./constants");
 
-//Required Middlewares #7
+//Required Middlewares #8
 const requiresDeviceFingerPrint = require("./middlewares/requires_device_finger_print");
 const requiresNoMaintenance = require("./middlewares/requires_no_maintenance");
 

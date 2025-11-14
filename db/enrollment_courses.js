f0ca21bd9886a13cf5e8131920d62af2a1b046c8
nisarg <hammerbyte.nisarg@gmail.com>
2025-11-15 03:49:08 +0530

    invoice support added

--- db/enrollment_courses.js
+++ db/enrollment_courses.js
@@ -4,7 +4,7 @@ const { logger } = require("sahas_utils");
 //freeze
 function getEnrollmentCoursesByEnrollmentId({ enrollment_id }) {
     return executeSQLQueryParameterized(
-        `SELECT ENROLLMENT_COURSES.*,USERS.full_name AS created_by_full_name FROM ENROLLMENT_COURSES LEFT JOIN USERS ON ENROLLMENT_COURSES.created_by=USERS.id WHERE ENROLLMENT_COURSES.enrollment_id=? ORDER BY ENROLLMENT_COURSES.id DESC`,
+        `SELECT ENROLLMENT_COURSES.*,USERS.full_name AS created_by_full_name,COURSES.title FROM ENROLLMENT_COURSES LEFT JOIN USERS ON ENROLLMENT_COURSES.created_by=USERS.id LEFT JOIN COURSES ON ENROLLMENT_COURSES.course_id=COURSES.id  WHERE ENROLLMENT_COURSES.enrollment_id=? ORDER BY ENROLLMENT_COURSES.id DESC`,
         [enrollment_id]
     ).catch((error) => {
         logger.error(`getEnrollmentCoursesByEnrollmentId: ${error}`);
--- routes/enrollment_transactions.js
+++ routes/enrollment_transactions.js
@@ -1,9 +1,13 @@
 const libExpress = require("express");
 const { validateRequestBody } = require("../utils");
-const { addEnrollmentTransaction, getEnrollmentTransactionById } = require("../db/enrollment_transactions");
+const { addEnrollmentTransaction, getEnrollmentTransactionById, updateEnrollmentTransactionInvoiceById } = require("../db/enrollment_transactions");
 const { readConfig } = require("../libs/config");
+const { requestService } = require("sahas_utils");
 const router = libExpress.Router();
-
+const libNumbersToWords = require("number-to-words");
+const { getEnrollmentCoursesByEnrollmentId } = require("../db/enrollment_courses");
+const { getEnrollmentById } = require("../db/enrollments");
+const { getUserById } = require("../db/users");
 //tested
 router.post("/", async (req, res) => {
     const requiredBodyFields = ["enrollment_id", "amount", "note", "type"];
@@ -20,7 +24,56 @@ router.post("/", async (req, res) => {
             sgst: (validatedRequestBody?.amount * sgst) / (100 + cgst + sgst),
         });
 
-        res.status(201).json(await getEnrollmentTransactionById({ id: enrollmentTransactionId }));
+        const enrollment = await getEnrollmentById({ id: validatedRequestBody.enrollment_id });
+        const enrollmentCourses = await getEnrollmentCoursesByEnrollmentId({ enrollment_id: validatedRequestBody.enrollment_id });
+        const enrollmentTransaction = await getEnrollmentTransactionById({ id: enrollmentTransactionId });
+        const enrollmentUser = await getUserById({ id: enrollment.user_id });
+
+        //generate invoice
+        await requestService({
+            requestServiceName: process.env.SERVICE_MEDIA,
+            onRequestStart: () => logger.info("Generating Invoice"),
+            requestPath: "templated/pdf",
+            requestMethod: "POST",
+            requestPostBody: {
+                template: "invoice",
+                injects: {
+                    invoice_date: getFormattedDate({ date: enrollmentTransaction.created_on, format: "DD-MM-YY" }), //
+                    transaction_id: enrollmentTransactionId, //
+                    course_title: enrollmentCourses.map({ title }).join(","), //
+
+                    user_name: `${enrollmentUser?.firstName} ${enrollmentUser?.lastName}`, //
+                    user_email: enrollmentUser?.email, //
+                    user_phone: enrollmentUser?.phone, //
+                    validity: getFormattedDate({ date: enrollment?.end_date, format: "DD-MM-YY" }), //
+
+                    payment_date: getFormattedDate({ date: enrollmentTransaction.created_on, format: "DD-MM-YY HH:mm:ss" }), //
+                    cgst_percentage: cgst, //
+                    sgst_percentage: sgst, //
+                    price_original: enrollmentTransaction?.original, //
+                    price_pre_tax: enrollmentTransaction?.amount, //
+                    discount: enrollmentTransaction?.discount, //
+                    coupon_code: "No Coupon Code", //
+                    total_tax: (Number(enrollmentTransaction?.cgst) + Number(enrollmentTransaction?.sgst)).toFixed(2), //
+                    cgst: enrollmentTransaction?.cgst, //
+                    sgst: enrollmentTransaction?.sgst, //
+                    price_pay: validatedRequestBody?.amount, //
+                    price_pay_words: libNumbersToWords.toWords(validatedRequestBody?.amount).toUpperCase(), //
+                },
+            },
+            onResponseReceieved: (generatedInvoice, responseCode) => {
+                if (generatedInvoice?.cdn_url && responseCode === 201) {
+                    logger.success(`Invoice For Transaction - ${enrollmentTransactionId} Generated !`);
+                    updateEnrollmentTransactionInvoiceById({ id: enrollmentTransactionId, invoice: generatedInvoice.cdn_url });
+                } else {
+                    logger.error(
+                        `Failed To Generate Invoice For Transaction - ${enrollmentTransactionId} - Media Responded With ${JSON.stringify(generatedInvoice)}`
+                    );
+                }
+            },
+        });
+
+        res.status(201).json(enrollmentTransaction);
     } else {
         res.status(400).json({ error: `Missing ${missingRequestBodyFields?.join(",")}` });
     }

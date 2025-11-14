493cf9bdbecdbd5d1575125cd07f72fde6e70d67
nisarg <hammerbyte.nisarg@gmail.com>
2025-11-15 01:57:28 +0530

    coupon fix

--- db/enrollment_transactions.js
+++ db/enrollment_transactions.js
@@ -13,7 +13,7 @@ function getTransactionsByEnrollmentId({ enrollment_id }) {
 }
 
 //freeze
-function addEnrollmentTransaction({ enrollment_id, amount, cgst, sgst, created_by, coupon_code, discount, note, type }) {
+function addEnrollmentTransaction({ enrollment_id, amount, cgst, sgst, created_by, coupon_code = null, discount = 0, note, type }) {
     return executeSQLQueryParameterized(
         `INSERT INTO ENROLLMENT_TRANSACTIONS(enrollment_id,amount,cgst,sgst,created_by,coupon_code,discount,note,type) VALUES(?,?,?,?,?,?,?,?,?)`,
         [enrollment_id, amount, cgst, sgst, created_by, coupon_code, discount, note, type]

b7dddc55c8ce9e7be736ad898317f3490c5f16cd
nisarg <hammerbyte.nisarg@gmail.com>
2025-11-19 17:39:07 +0530

    subject fetch

--- routes/subjects.js
+++ routes/subjects.js
@@ -28,6 +28,15 @@ router.get("/", async (req, res) => {
 });
 
 //tested
+router.get("/:id", async (req, res) => {
+    if (!req.params.id) {
+        return res.status(400).json({ error: "Missing Subject id" });
+    }
+    //provide subject
+    res.status(200).json(await getCourseSubjectById({ id: req.params.id }));
+});
+
+//tested
 router.get("/:id/chapters", async (req, res) => {
     if (!req.params.id) {
         return res.status(400).json({ error: "Missing Subject id" });

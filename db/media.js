13450d4bfb29d2ec9ee10702d1aeb574768ea769
nisarg <hammerbyte.nisarg@gmail.com>
2025-11-21 02:57:32 +0530

    dev deploy

--- db/media.js
+++ db/media.js
@@ -7,4 +7,16 @@ function getMediaByChapterId({ chapter_id }) {
     );
 }
 
-module.exports = { getMediaByChapterId };
+function getMediaById({ id }) {
+    return executeSQLQueryParameterized(`SELECT * FROM CHAPTER_MEDIA WHERE id=? `, [id])
+        .then((result) => (result.length > 0 ? result[0] : false))
+        .catch((error) => logger.error(`getMediaById: ${error}`));
+}
+
+function addMedia({ chapter_id, title, cdn_url, type }) {
+    return executeSQLQueryParameterized(`INSERT INTO CHAPTER_MEDIA (chapter_id,title,cdn_url,type)`, [chapter_id, title, cdn_url, type])
+        .then((result) => result.insertId)
+        .catch((error) => logger.error(`addMedia: ${error}`));
+}
+
+module.exports = { getMediaByChapterId, addMedia, getMediaById };
---
+++ routes/media.js
@@ -0,0 +1,31 @@
+const libExpress = require("express");
+const { addInquiryNote, getInquiryNoteById, deleteInquiryNoteById } = require("../db/inquiry_notes");
+const { validateRequestBody } = require("../utils");
+const { addMedia, getMediaById } = require("../db/media");
+
+const router = libExpress.Router();
+
+//tested
+router.post("/", async (req, res) => {
+    const requiredBodyFields = ["chapter_id", "title", "cdn_url", "type"];
+
+    const { isRequestBodyValid, missingRequestBodyFields, validatedRequestBody } = validateRequestBody(req.body, requiredBodyFields);
+
+    if (isRequestBodyValid) {
+        const mediaId = await addMedia({ ...validatedRequestBody, created_by: req.user.id });
+        res.status(201).json(await getMediaById({ id: mediaId }));
+    } else {
+        res.status(400).json({ error: `Missing ${missingRequestBodyFields?.join(",")}` });
+    }
+});
+
+//tested
+router.delete("/:id", async (req, res) => {
+    if (!req.params.id) {
+        return res.status(400).json({ error: "Missing inquiryNoteId" });
+    }
+    deleteInquiryNoteById({ id: req.params.id });
+    res.sendStatus(204);
+});
+
+module.exports = router;

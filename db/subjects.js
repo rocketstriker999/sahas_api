18dee08a97fff92ed129e08fd5799b3f36fc1ab0
nisarg <hammerbyte.nisarg@gmail.com>
2025-12-01 14:19:47 +0530

    background_color fix

--- db/subjects.js
+++ db/subjects.js
@@ -2,14 +2,14 @@ const { executeSQLQueryParameterized } = require("../libs/db");
 const { logger } = require("sahas_utils");
 
 //freeze
-function addSubject({ title, background_color }) {
+function addSubject({ title, background_color = null }) {
     return executeSQLQueryParameterized(`INSERT INTO SUBJECTS(title,background_color) VALUES(?,?)`, [title, background_color])
         .then((result) => result.insertId)
         .catch((error) => logger.error(`addSubject: ${error}`));
 }
 
 //freeze
-function updateSubjectById({ id, title, background_color }) {
+function updateSubjectById({ id, title, background_color = null }) {
     return executeSQLQueryParameterized(`UPDATE SUBJECTS SET title=?,background_color=? WHERE id=?`, [title, background_color, id]).catch((error) =>
         logger.error(`updateSubjectById: ${error}`)
     );

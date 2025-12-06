7ec555f78354cd6f6a997f40126be18646d1602f
nisarg <hammerbyte.nisarg@gmail.com>
2025-12-07 02:55:11 +0530

    dev

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const { generateDBTables } = require("./libs/db");
 const { prepareDirectories } = require("./utils");
 const { generateCaches } = require("./libs/cacher");
 
-//prepare logs directories at first #11
+//prepare logs directories at first #12
 prepareDirectories([process.env.DIRECTORY_LOGS, process.env.DIR_CONFIGS]);
 
 // Test and Prepare Required Tables

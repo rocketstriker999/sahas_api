acbbb04ce025c627f9a0799dfa2ef36650a872fe
nisarg <hammerbyte.nisarg@gmail.com>
2025-12-22 21:44:35 +0530

    tables generation

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const { logger } = require("sahas_utils");
 const cors = require("cors");
 const { ROUTE_NOT_FOUND } = require("./constants");
 
-//Required Middlewares #1
+//Required Middlewares #2
 const requiresDeviceFingerPrint = require("./middlewares/requires_device_finger_print");
 const requiresNoMaintenance = require("./middlewares/requires_no_maintenance");
 

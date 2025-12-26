d04447913a0eb18872c1c4b56262e34d908ef9ee
nisarg <hammerbyte.nisarg@gmail.com>
2025-12-27 04:46:18 +0530

    device fux

--- middlewares/log_request.js
+++ middlewares/log_request.js
@@ -2,7 +2,7 @@ const { logger } = require("sahas_utils");
 
 module.exports = async (req, res, next) => {
     logger.info(
-        `Incoming Request - ${req.method} ${req.url} | USER_EMAIL : ${req?.user?.email} | Device : ${req?.device?.description} | Device Active : ${req.device.active}`
+        `Incoming Request - ${req.method} ${req.url} | USER_EMAIL : ${req?.user?.email} | Device : ${req?.device?.description} | Device Active : ${req?.device?.active}`
     );
 
     next();

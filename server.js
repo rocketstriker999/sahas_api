7175674b0b7f73ab5f44593310b1cb93e1273180
nisarg <hammerbyte.nisarg@gmail.com>
2025-12-27 04:51:31 +0530

    cors add

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const logRequest = require("./middlewares/log_request");
 const sahasAPI = libExpress();
 
 // Use the CORS middleware to allow cross origin request in case of testing UI Localhost and Cookies as well
-sahasAPI.use(cors({ origin: process.env.ALLOWED_CORS_ORIGINS }));
+sahasAPI.use(cors({ origin: process.env.ALLOWED_CORS_ORIGINS ? process.env.ALLOWED_CORS_ORIGINS.split(",") : [] }));
 
 //allow json request payloads and cookies only by express
 sahasAPI.use(libExpress.json());

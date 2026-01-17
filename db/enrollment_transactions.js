ebf4eb78814befe3bb630928a4901d83c78b700c
nisarg <hammerbyte.nisarg@gmail.com>
2026-01-17 16:49:58 +0530

    dev

--- db/enrollment_transactions.js
+++ db/enrollment_transactions.js
@@ -5,7 +5,7 @@ const { logger } = require("sahas_utils");
 function getTransactionsByEnrollmentId({ enrollment_id }) {
     return executeSQLQueryParameterized(
         `SELECT ENROLLMENT_TRANSACTIONS.*,USERS.full_name AS created_by_full_name FROM ENROLLMENT_TRANSACTIONS LEFT JOIN USERS ON ENROLLMENT_TRANSACTIONS.created_by=USERS.id WHERE enrollment_id=?`,
-        [enrollment_id]
+        [enrollment_id],
     ).catch((error) => {
         logger.error(`getTransactionsByEnrollmentId: ${error}`);
         return [];
@@ -16,7 +16,7 @@ function getTransactionsByEnrollmentId({ enrollment_id }) {
 function addEnrollmentTransaction({ enrollment_id, amount, cgst, sgst, created_by, coupon_code = null, discount = 0, note, type, image = null }) {
     return executeSQLQueryParameterized(
         `INSERT INTO ENROLLMENT_TRANSACTIONS(enrollment_id,amount,cgst,sgst,created_by,coupon_code,discount,note,type,image) VALUES(?,?,?,?,?,?,?,?,?,?)`,
-        [enrollment_id, amount, cgst, sgst, created_by, coupon_code, discount, note, type, image]
+        [enrollment_id, amount, cgst, sgst, created_by, coupon_code, discount, note, type, image],
     )
         .then((result) => result.insertId)
         .catch((error) => logger.error(`addEnrollmentTransaction: ${error}`));
@@ -25,7 +25,7 @@ function addEnrollmentTransaction({ enrollment_id, amount, cgst, sgst, created_b
 //freeze
 function updateEnrollmentTransactionInvoiceById({ id, invoice }) {
     return executeSQLQueryParameterized(`UPDATE ENROLLMENT_TRANSACTIONS SET invoice=? where id=?`, [invoice, id]).catch((error) =>
-        logger.error(`updateEnrollmentTransactionInvoiceById: ${error}`)
+        logger.error(`updateEnrollmentTransactionInvoiceById: ${error}`),
     );
 }
 
@@ -33,7 +33,7 @@ function updateEnrollmentTransactionInvoiceById({ id, invoice }) {
 function getEnrollmentTransactionById({ id }) {
     return executeSQLQueryParameterized(
         "SELECT ENROLLMENT_TRANSACTIONS.*,USERS.full_name AS created_by_full_name FROM ENROLLMENT_TRANSACTIONS LEFT JOIN USERS ON ENROLLMENT_TRANSACTIONS.created_by = USERS.id WHERE ENROLLMENT_TRANSACTIONS.id=? ",
-        [id]
+        [id],
     )
         .then((result) => (result.length > 0 ? result[0] : false))
         .catch((error) => logger.error(`getEnrollmentTransactionById: ${error}`));
@@ -42,8 +42,8 @@ function getEnrollmentTransactionById({ id }) {
 //freeze
 function getEnrollmentTransactionsForInterval({ start_date, end_date, order_by = "DESC" }) {
     return executeSQLQueryParameterized(
-        `SELECT ENROLLMENT_TRANSACTIONS.id,ENROLLMENT_TRANSACTIONS.enrollment_id,ENROLLMENTS.user_id,ENROLLMENTS.handler, ENROLLMENT_TRANSACTIONS.amount, ENROLLMENT_TRANSACTIONS.created_on, USERS.full_name FROM ENROLLMENT_TRANSACTIONS LEFT JOIN ENROLLMENTS ON ENROLLMENT_TRANSACTIONS.enrollment_id = ENROLLMENTS.id LEFT JOIN USERS ON ENROLLMENTS.user_id = USERS.id WHERE ENROLLMENT_TRANSACTIONS.created_on BETWEEN ? AND ? ORDER BY ENROLLMENT_TRANSACTIONS.created_on ${order_by}`,
-        [start_date, end_date]
+        `SELECT ENROLLMENT_TRANSACTIONS.id,ENROLLMENT_TRANSACTIONS.enrollment_id,ENROLLMENTS.user_id,ENROLLMENTS.handler, ENROLLMENT_TRANSACTIONS.amount,ENROLLMENT_TRANSACTIONS.type, ENROLLMENT_TRANSACTIONS.created_on, USERS.full_name FROM ENROLLMENT_TRANSACTIONS LEFT JOIN ENROLLMENTS ON ENROLLMENT_TRANSACTIONS.enrollment_id = ENROLLMENTS.id LEFT JOIN USERS ON ENROLLMENTS.user_id = USERS.id WHERE ENROLLMENT_TRANSACTIONS.created_on BETWEEN ? AND ? ORDER BY ENROLLMENT_TRANSACTIONS.created_on ${order_by}`,
+        [start_date, end_date],
     ).catch((error) => logger.error(`getEnrollmentTransactionsForInterval: ${error}`));
 }
 

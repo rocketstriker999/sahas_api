4c02296ebe05603e29676fe7acf6f370bb1280ec
nisarg <hammerbyte.nisarg@gmail.com>
2026-03-13 16:00:46 +0530

    inquiries filters

--- routes/filters.js
+++ routes/filters.js
@@ -28,10 +28,11 @@ router.get("/users", async (req, res) => {
 
 //filters for inquiry management
 router.get("/inquiries", async (req, res) => {
-    res.status(200).json({
-        active: booleanFilters,
-        id: sortFilters,
-    });
+    res.status(200).json([
+        { title: "Status Active", key: "active", options: booleanFilters, type: "MULTI_SELECT" },
+        { title: "Fees Due", key: "dues", options: comparisionFilters, type: "DROP_DOWN" },
+        { title: "Sort Order", key: "id", options: sortFilters, type: "DROP_DOWN" },
+    ]);
 });
 
 router.get("/reports", async (req, res) => {

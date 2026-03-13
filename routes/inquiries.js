15530f824a028d4bc905200d029eea40b8d9ae42
nisarg <hammerbyte.nisarg@gmail.com>
2026-03-13 22:32:46 +0530

    dev

--- routes/inquiries.js
+++ routes/inquiries.js
@@ -64,6 +64,7 @@ router.get("/download", async (req, res) => {
     for (const inquiry of inquiries) {
         if (!!inquiry?.branch_id) {
             inquiry.branch = branchSelector[inquiry?.branch_id];
+            inquiry.course = courseSelector[inquiry?.course_id];
         }
     }
 

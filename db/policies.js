4a2061b6917b4ab28872410e7cf94bab410abebd
nisarg <hammerbyte.nisarg@gmail.com>
2026-03-24 01:02:31 +0530

    dev

--- db/policies.js
+++ db/policies.js
@@ -3,16 +3,23 @@ const { logger } = require("sahas_utils");
 
 // Get all policies
 function getAllPolicies() {
-    return executeSQLQueryParameterized("SELECT * FROM POLICIES ORDER BY created_at DESC").catch((error) => {
+    return executeSQLQueryParameterized("SELECT * FROM POLICIES ORDER BY view_index ASC").catch((error) => {
         logger.error(`getAllPolicies: ${error}`);
         return [];
     });
 }
 
 // Update policy by ID
+function addPolicy({ title, description }) {
+    return executeSQLQueryParameterized("INSERT INTO POLICIES (title, description) VALUES(?,?)", [title, description]).catch((error) =>
+        logger.error(`addPolicy: ${error}`),
+    );
+}
+
+// Update policy by ID
 function updatePolicyById({ id, title, description }) {
     return executeSQLQueryParameterized("UPDATE POLICIES SET title=?, description=? WHERE id=?", [title, description, id]).catch((error) =>
-        logger.error(`updatePolicyById: ${error}`)
+        logger.error(`updatePolicyById: ${error}`),
     );
 }
 
@@ -25,9 +32,14 @@ function getPolicyById({ id }) {
 
 // Delete policy by ID
 function deletePolicyById({ id }) {
-    return executeSQLQueryParameterized("DELETE FROM POLICIES WHERE id=?", [id]).catch((error) =>
-        logger.error(`deletePolicyById: ${error}`)
+    return executeSQLQueryParameterized("DELETE FROM POLICIES WHERE id=?", [id]).catch((error) => logger.error(`deletePolicyById: ${error}`));
+}
+
+//freeze
+function updatePolicyViewIndexById({ id, view_index }) {
+    return executeSQLQueryParameterized("UPDATE POLICIES SET view_index=? WHERE id=?", [view_index, id]).catch((error) =>
+        logger.error(`updatePolicyViewIndexById: ${error}`),
     );
 }
 
-module.exports = { getAllPolicies, updatePolicyById, getPolicyById, deletePolicyById };
+module.exports = { getAllPolicies, updatePolicyById, getPolicyById, deletePolicyById, addPolicy, updatePolicyViewIndexById };
--- routes/policies.js
+++ routes/policies.js
@@ -1,6 +1,7 @@
 const libExpress = require("express");
-const { getAllPolicies, updatePolicyById, deletePolicyById, getPolicyById } = require("../db/policies");
+const { getAllPolicies, updatePolicyById, deletePolicyById, getPolicyById, addPolicy, updatePolicyViewIndexById } = require("../db/policies");
 const { validateRequestBody } = require("sahas_utils");
+const { AUTHORITIES } = require("../constants");
 
 const router = libExpress.Router();
 
@@ -10,8 +11,22 @@ router.get("/", async (req, res) => {
     res.status(200).json(policies);
 });
 
+// Post the Policy
+router.post("/", requires_authority(AUTHORITIES.CREATE_POLICY), async (req, res) => {
+    const requiredBodyFields = ["id", "title", "description"];
+
+    const { isRequestBodyValid, missingRequestBodyFields, validatedRequestBody } = validateRequestBody(req.body, requiredBodyFields);
+
+    if (isRequestBodyValid) {
+        await addPolicy(validatedRequestBody);
+        res.status(201).json(await getPolicyById({ id: validatedRequestBody.id }));
+    } else {
+        res.status(400).json({ error: `Missing ${missingRequestBodyFields?.join(",")}` });
+    }
+});
+
 // Patch the Policy
-router.patch("/", async (req, res) => {
+router.patch("/", requires_authority(AUTHORITIES.UPDATE_POLICY), async (req, res) => {
     const requiredBodyFields = ["id", "title", "description"];
 
     const { isRequestBodyValid, missingRequestBodyFields, validatedRequestBody } = validateRequestBody(req.body, requiredBodyFields);
@@ -25,12 +40,22 @@ router.patch("/", async (req, res) => {
 });
 
 // Delete the Policy
-router.delete("/:id", async (req, res) => {
+router.delete("/:id", requires_authority(AUTHORITIES.DELETE_POLICY), async (req, res) => {
     if (!req.params.id) {
         return res.status(400).json({ error: "Missing Policy Id" });
     }
     await deletePolicyById({ id: req.params.id });
     res.sendStatus(204);
+});
+
+//tested
+router.patch("/view_indexes", requires_authority(AUTHORITIES.UPDATE_POLICY_VIEW_INDEX), async (req, res) => {
+    if (req.body?.length) {
+        req.body.forEach(updatePolicyViewIndexById);
+        return res.sendStatus(200);
+    }
+
+    return res.status(400).json({ error: "Missing Courses" });
 });
 
 module.exports = router;

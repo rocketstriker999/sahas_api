8155eeb82ea821e9f27cc706078c1b4ef600158f
nisarg <hammerbyte.nisarg@gmail.com>
2026-03-24 01:13:39 +0530

    dev

--- libs/db.js
+++ libs/db.js
@@ -300,6 +300,7 @@ async function generateDBTables() {
             id INT AUTO_INCREMENT PRIMARY KEY,
             title VARCHAR(255) NOT NULL,
             description VARCHAR(256),
+            view_index INT NOT NULL DEFAULT 0,
             created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
             updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
         )`,

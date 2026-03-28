6657c0f3adac7ada5404e3fb4467cb102a29559e
nisarg <hammerbyte.nisarg@gmail.com>
2026-03-29 03:28:46 +0530

    tiny fix

--- db/stream_selection_tests.js
+++ db/stream_selection_tests.js
@@ -8,10 +8,12 @@ function addStreamSelectionTest({ user_id }) {
         .catch((error) => logger.error(`addStreamSelectionTest: ${error}`));
 }
 
-function addStreamSelectionTestAnswer({ stream_selection_test_id, user_id }) {
-    return executeSQLQueryParameterized("INSERT INTO STREAM_SELECTION_TEST_ANSWERS(user_id) VALUES(?)", [user_id])
-        .then((result) => result.insertId)
-        .catch((error) => logger.error(`addStreamSelectionTest: ${error}`));
+function addStreamSelectionTestAnswer({ stream_selection_test_id, question, answer }) {
+    return executeSQLQueryParameterized("INSERT INTO STREAM_SELECTION_TEST_ANSWERS(stream_selection_test_id,question,answer) VALUES(?)", [
+        stream_selection_test_id,
+        question,
+        answer,
+    ]).catch((error) => logger.error(`addStreamSelectionTestAnswer: ${error}`));
 }
 
 module.exports = {

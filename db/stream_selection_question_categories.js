0c435441194b34ff0163addc3ebf4ccba7dd4040
nisarg <hammerbyte.nisarg@gmail.com>
2026-04-03 02:35:16 +0530

    getAllStreamSelectionQuestionCategories

--- db/stream_selection_question_categories.js
+++ db/stream_selection_question_categories.js
@@ -2,10 +2,10 @@ const { executeSQLQueryParameterized } = require("../libs/db");
 const { logger } = require("sahas_utils");
 
 //freeze
-function getAllStreamSelectionQuestionCategories({ question }) {
-    return executeSQLQueryParameterized("SELECT * FROM STREAM_SELECTION_QUESTION_CATEGORIES ORDER BY view_index ASC", [question])
-        .then((result) => result.insertId)
-        .catch((error) => logger.error(`addStreamSelectionQuestion: ${error}`));
+function getAllStreamSelectionQuestionCategories() {
+    return executeSQLQueryParameterized("SELECT * FROM STREAM_SELECTION_QUESTION_CATEGORIES ORDER BY view_index ASC").catch((error) =>
+        logger.error(`getAllStreamSelectionQuestionCategories: ${error}`),
+    );
 }
 
 module.exports = {

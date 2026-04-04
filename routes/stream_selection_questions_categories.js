5615ae3eb80503067daba76481a6b43e21185635
nisarg <hammerbyte.nisarg@gmail.com>
2026-04-05 02:03:10 +0530

    category

--- routes/stream_selection_questions_categories.js
+++ routes/stream_selection_questions_categories.js
@@ -62,10 +62,8 @@ router.put("/", requires_authority(AUTHORITIES.UPDATE_STREAM_SELECTION_QUESTION_
 
     if (isRequestBodyValid) {
         await updateStreamSelectionQuestionCategoryById(validatedRequestBody);
-
-        const category = await getStreamSelectionQuestionCategoryById({ id: validatedRequestBody?.id });
+        const category = await getStreamSelectionQuestionCategoryById(validatedRequestBody);
         category.questions = await getStreamSelectionQuestionsCountByCategoryId({ category_id: category?.id });
-
         res.status(200).json(category);
     } else {
         res.status(400).json({ error: `Missing ${missingRequestBodyFields?.join(",")}` });

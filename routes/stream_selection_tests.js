ef118b7432ae0256396ab3fd33cbd22dbcbb6050
nisarg <hammerbyte.nisarg@gmail.com>
2026-04-06 01:58:23 +0530

    timeout

--- routes/stream_selection_tests.js
+++ routes/stream_selection_tests.js
@@ -27,7 +27,9 @@ router.post("/", async (req, res) => {
         //update user that stream selection test is taken
         updateStreamSelectionTestByUserId({ stream_selection_test_taken: true, user_id: req.user.id });
 
-        return setTimeout(() => res.sendStatus(201), 10000);
+        await setTimeout(() => {
+            return res.sendStatus(201);
+        }, 10000);
     }
 
     return res.status(400).json({ error: "Missing Test Questions" });
